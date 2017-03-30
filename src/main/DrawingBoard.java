2953fd4fdb702d422ae8e204b9047d665a0ba5be
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-03-30 12:08:37 +0700

    first commit

---
+++ src/main/DrawingBoard.java
@@ -0,0 +1,124 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+import objects.*;
+
+public class DrawingBoard extends JPanel {
+
+	private MouseAdapter mouseAdapter; 
+	private List<GObject> gObjects;
+	private GObject target;
+
+	private int gridSize = 10;
+
+	public DrawingBoard() {
+		gObjects = new ArrayList<GObject>();
+		mouseAdapter = new MAdapter();
+		addMouseListener(mouseAdapter);
+		addMouseMotionListener(mouseAdapter);
+		setPreferredSize(new Dimension(800, 600));
+	}
+
+	public void addGObject(GObject gObject) {
+		gObjects.add(gObject);
+		repaint();
+	}
+
+	public void groupAll() {
+		CompositeGObject group = new CompositeGObject();
+		for(GObject g : gObjects ){
+			group.add(g);
+			gObjects.remove(g);
+		}
+		gObjects.add(group);
+		repaint();
+	}
+
+	public void deleteSelected() {
+		for( GObject g : gObjects ){
+			if(g.isSelected()){
+				gObjects.remove(g);
+				break;
+			}
+		}
+		repaint();
+	}
+
+	public void clear() {
+		gObjects.clear();
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		paintBackground(g);
+		paintGrids(g);
+		paintObjects(g);
+	}
+
+	private void paintBackground(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	private void paintGrids(Graphics g) {
+		g.setColor(Color.lightGray);
+		int gridCountX = getWidth() / gridSize;
+		int gridCountY = getHeight() / gridSize;
+		for (int i = 0; i < gridCountX; i++) {
+			g.drawLine(gridSize * i, 0, gridSize * i, getHeight());
+		}
+		for (int i = 0; i < gridCountY; i++) {
+			g.drawLine(0, gridSize * i, getWidth(), gridSize * i);
+		}
+	}
+
+	private void paintObjects(Graphics g) {
+		for (GObject go : gObjects) {
+			go.paint(g);
+		}
+	}
+
+	class MAdapter extends MouseAdapter {
+
+		// TODO: You need some variables here
+
+		private void deselectAll() {
+			// TODO: Implement this method.
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.err.println("press");
+			for( GObject g : gObjects ){
+				g.deselected();
+			}
+			for( GObject g : gObjects ){
+				if(g.pointerHit(e.getX(), e.getY())){
+					g.selected();
+					break;
+				}		
+			}
+			repaint();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println("Drag");
+			for( GObject g : gObjects ){
+				if(g.isSelected()){
+					g.move(e.getX(), e.getY());
+					break;
+				}
+
+			}
+			repaint();
+		}
+	}
+
+}
